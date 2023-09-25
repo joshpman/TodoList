c8e8f64c46ea5533cf5124390b69ff5b1979bbea
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-09-25 08:23:16 -0400

    Basic Restyling

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addItem(){
         const newItem = document.querySelector(".test").cloneNode(true);
         newItem.querySelector(".test__input").placeholder=item;
         document.getElementsByClassName("item__list")[0].appendChild(newItem);
-        
+        document.getElementsByClassName("test__input")[0].addEventListener('click', strike());
     }
 }
 
@@ -52,7 +52,12 @@ function clearAll(){
     let listItems = document.querySelector(".item__list");
     let arr = Array.from(listItems.querySelectorAll("li"));
     for(let i = 1; i<arr.length; i++){
-        console.log("Bump");
         arr[i].remove();
     }
+}
+
+function strike(event){
+   // const target = event.target;
+    //const listItem = target.closest('.test');
+    console.log(listItem);
 }
